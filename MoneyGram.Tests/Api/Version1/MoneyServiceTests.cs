feedd6256d81f7db85e0f4148108ae74a34b53d3
sharath <sharaththokala@hotmail.com>
2019-04-16 18:54:38 +0100

    Unit Tests Added

---
+++ MoneyGram.Tests/Api/Version1/MoneyServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using Moq;
+using MovieGram.Api.Controllers.Version1.Models;
+using MovieGram.Api.Controllers.Version1.Services;
+using MovieGram.Core.Filters;
+using MovieGram.Data;
+using MovieGram.Data.Repository;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MoneyGram.Tests.Api.Version1
+{
+    public class MoneyServiceTests
+    {
+        [Fact]
+        public void SearchMovieMapsToModel()
+        {
+            //Arrange
+            var movieList = new List<Movie> {
+                new Movie { Id = 1, Name = "Movie1", Plot = "Plot1", Rating = 4, FullImageUrl = "http://test.com/content/images/Movie1.png", ThumbnailUrl = "http://test.com/content/thumbs/Movie1.png",
+                    ShowTimes = new List<ShowTime> { new ShowTime { Id = 1, MovieId = 1, CinemaId = 1, Time = new DateTime(2019, 5, 1, 14, 0, 0, 0), Cinema = new Cinema { Id = 1, Name = "Cinema1" } },
+                                                     new ShowTime { Id = 2, MovieId = 1, CinemaId = 2, Time = new DateTime(2019, 6, 2, 14, 0, 0, 0),Cinema = new Cinema { Id = 2, Name = "Cinema2" } } }
+                }
+            };
+
+            var mockMovieRepository = new Mock<IMovieRepository>();
+            mockMovieRepository.Setup(x => x.GetFilteredMovies(It.IsAny<MovieFilter>())).Returns(movieList);
+
+            var movieService = new MovieService(mockMovieRepository.Object);
+
+            //Act
+            var result = movieService.SearchMovie("movie");
+
+            //Assert
+            result.Should().NotBeEmpty()
+               .And.HaveCount(1)
+               .And.ContainItemsAssignableTo<MovieListItemModel>()
+               .And.Subject.Should().AllBeEquivalentTo(new MovieListItemModel
+               {
+                   Id = movieList[0].Id,
+                   ThumbnailUrl = movieList[0].ThumbnailUrl,
+                   Name = movieList[0].Name,
+                   ShowTimes = new List<ShowTimeModel> { new ShowTimeModel { CinemaName = "Cinema1", Time = new DateTime(2019, 5, 1, 14, 0, 0, 0) }, new ShowTimeModel { CinemaName = "Cinema2", Time = new DateTime(2019, 6, 2, 14, 0, 0, 0) } }
+               });
+            mockMovieRepository.Verify(x => x.GetFilteredMovies(It.IsAny<MovieFilter>()), Times.Once);
+        }
+
+        [Fact]
+        public void WhenNoMovieRecordsFoundSearchMovieShouldReturnNull()
+        {
+            //Arrange
+            var mockMovieRepository = new Mock<IMovieRepository>();
+            mockMovieRepository.Setup(x => x.GetFilteredMovies(It.IsAny<MovieFilter>())).Returns<List<Movie>>(null);
+
+            var movieService = new MovieService(mockMovieRepository.Object);
+
+            //Act
+            var result = movieService.SearchMovie("movie");
+
+            //Assert
+            result.Should().BeNull();
+            mockMovieRepository.Verify(x => x.GetFilteredMovies(It.IsAny<MovieFilter>()), Times.Once);
+        }
+    }
+}
---
+++ MovieGram.Data/Migrations/20190416175044_InitialCreate.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieGram.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cinema",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cinema", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    ThumbnailUrl = table.Column<string>(nullable: true),
+                    FullImageUrl = table.Column<string>(nullable: true),
+                    Plot = table.Column<string>(nullable: true),
+                    Rating = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShowTime",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MovieId = table.Column<int>(nullable: false),
+                    CinemaId = table.Column<int>(nullable: false),
+                    Time = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShowTime", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShowTime_Cinema_CinemaId",
+                        column: x => x.CinemaId,
+                        principalTable: "Cinema",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ShowTime_Movie_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movie",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cinema",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Cinema1" },
+                    { 2, "Cinema2" },
+                    { 3, "Cinema3" },
+                    { 4, "Cinema4" },
+                    { 5, "Cinema5" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movie",
+                columns: new[] { "Id", "FullImageUrl", "Name", "Plot", "Rating", "ThumbnailUrl" },
+                values: new object[,]
+                {
+                    { 1, "http://test.com/content/images/Movie1.png", "Movie1", "Plot1", 4, "http://test.com/content/thumbs/Movie1.png" },
+                    { 2, "http://test.com/content/images/Movie2.png", "Movie2", "Plot2", 3, "http://test.com/content/thumbs/Movie2.png" },
+                    { 3, "http://test.com/content/images/Movie3.png", "Movie3", "Plot3", 2, "http://test.com/content/thumbs/Movie3.png" },
+                    { 4, "http://test.com/content/images/Movie4.png", "Movie4", "Plot4", 1, "http://test.com/content/thumbs/Movie4.png" },
+                    { 5, "http://test.com/content/images/Movie5.png", "Movie5", "Plot5", 5, "http://test.com/content/thumbs/Movie5.png" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ShowTime",
+                columns: new[] { "Id", "CinemaId", "MovieId", "Time" },
+                values: new object[,]
+                {
+                    { 1, 1, 1, new DateTime(2019, 5, 1, 14, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, 1, 1, new DateTime(2019, 6, 1, 14, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, 1, 1, new DateTime(2019, 7, 1, 14, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 4, 5, 2, new DateTime(2019, 5, 1, 14, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 5, 4, 3, new DateTime(2019, 5, 1, 14, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 6, 3, 3, new DateTime(2019, 5, 1, 14, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 7, 4, 4, new DateTime(2019, 5, 1, 14, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 8, 3, 4, new DateTime(2019, 6, 1, 14, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 9, 2, 4, new DateTime(2019, 6, 1, 14, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 10, 2, 5, new DateTime(2019, 6, 1, 14, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShowTime_CinemaId",
+                table: "ShowTime",
+                column: "CinemaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShowTime_MovieId",
+                table: "ShowTime",
+                column: "MovieId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShowTime");
+
+            migrationBuilder.DropTable(
+                name: "Cinema");
+
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
